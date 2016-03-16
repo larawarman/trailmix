3bac3e48c3b280d0c77b58e1748f536c9111ce65
larawarman <lara.warman@gmail.com>
2016-03-16 13:46:43 -0400

    require react fire and firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
 
 var Hello = React.createClass({
   render: function() {
