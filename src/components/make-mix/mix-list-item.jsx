ccd55d61046501c64623fc10fb5d353f9f65570f
larawarman <lara.warman@gmail.com>
2016-03-24 10:28:30 -0400

    reordering! but delete broke...

--- src/components/make-mix/mix-list-item.jsx
+++ src/components/make-mix/mix-list-item.jsx
@@ -1,28 +1,33 @@
 var React = require('react');
 var Firebase = require('firebase');
 var fireUrl = 'https://trailmix0.firebaseio.com/';
-// var Sortable = require('react-sortable-items');
-// var SortableMixin = require('react-sortable-items/SortableItemMixin');
+var Sortable = require('react-sortable-items');
+var SortableItemMixin = require('react-sortable-items/SortableItemMixin');
 
 module.exports = React.createClass({
-  // mixins: [SortableMixin],
+  mixins: [SortableItemMixin],
   componentWillMount: function() {
-    this.fbsong = new Firebase(fireUrl + 'mixes/mix/songs/' + this.props.song.key);
+    this.fbsong = new Firebase(fireUrl + 'mixes/mix/songs/' + this.props.key);
   },
   getInitialState: function(){
     return {
-      track_name: this.props.song.track_name,
-      artistJoined: this.props.song.artistJoined,
-      id: this.props.song.id
+      track_name: this.props.title,
+      key: this.props.key
+      // artistJoined: this.props.song.artistJoined,
+      //id: this.props.song.id
     }
   },
   render: function() {
-    return <div key={this.props.id} className="list-item">
-      {this.state.track_name} by {this.state.artistJoined}
+    return this.renderWithSortable(<li key={this.props.key} className="list-item">
+      {this.props.title}
       <span onClick={this.handleDelete} className="delete">DELETE</span>
-    </div>
+    </li>);
   },
   handleDelete:function() {
+    console.log('delete');
     this.fbsong.remove();
+  },
+  handleDataLoaded:function(){
+    console.log("loaded");
   }
 });
--- src/components/make-mix/mix-list.jsx
+++ src/components/make-mix/mix-list.jsx
@@ -1,15 +1,19 @@
 var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
-var update = require('react-addons-update');
+var Sortable = require('react-sortable-items');
+var SortableItemMixin = require('react-sortable-items/SortableItemMixin');
 
 var MixListItem = require('./mix-list-item');
 
 var fireUrl = 'https://trailmix0.firebaseio.com/';
 
-
 module.exports = React.createClass({
-  mixins: [ ReactFire ],
+  mixins: [ ReactFire, SortableItemMixin ],
+  sortableOptions: {
+    ref: "songlist",
+    model: "songs"
+  },
   componentWillMount: function() {
     this.fbsonglist = new Firebase(fireUrl + '/mixes/mix/songs');
     this.bindAsObject(this.fbsonglist, 'mixSongs');
@@ -20,7 +24,7 @@ module.exports = React.createClass({
     return {
       loaded: false,
       mixSongs: {},
-      list: []
+      songs: []
     }
   },
   render: function() {
@@ -31,22 +35,38 @@ module.exports = React.createClass({
     </div>
   },
   renderMixSongs: function() {
-    var songs = [];
-    for (var key in this.state.mixSongs)  {
-      var song = this.state.mixSongs[key];
-      song.key = key;
-      songs.push(
-        <MixListItem
-          song={song}
-          key={key}
-        />
-      );
+    if(this.state.loaded){
+      for (var key in this.state.mixSongs) {
+        var song = this.state.mixSongs[key];
+        song.key = key;
+        this.state.songs.push(
+          song
+        );
+      }      
+      var songs = this.state.songs.map(function (song, idx) {
+        return <MixListItem 
+          key={song.key} 
+          title={song.track_name}
+          index={idx} 
+          sortData={idx} 
+          isDraggable={true} />;
+      });
+      return <div className="mix-area">
+        <Sortable onSort={this.handleSort}>
+          {songs}
+        </Sortable>
+      </div>
     }
-    return <div className="mix-area">
-      {songs}
-    </div>
   },
   handleDataLoaded: function(snapshot) {
     this.setState({loaded: true,});
+  },
+  handleSort: function(reorder) {
+    console.log(reorder);
+    // this.setState({
+    //   songs: reorder.map(function (idx) {
+    //     console.log(this.state.songs[idx]);
+    //   }.bind(this))
+    // });
   }
 });
