4498444e5f5a3dcddd6ffb301e7d4afa93d247d7
larawarman <lara.warman@gmail.com>
2016-04-12 23:16:39 -0400

    have the song queue, need to figure out how to hook in next/prev

--- src/components/listen/audio-player.jsx
+++ src/components/listen/audio-player.jsx
@@ -5,23 +5,95 @@ var StateMixin = require('reflux-state-mixin');
 
 var Actions = require('../../actions');
 var AudioStore = require('../../stores/audioPlayer-store');
+var fireUrl = 'https://trailmix0.firebaseio.com/';
+
 
 module.exports = React.createClass({
   mixins: [
     StateMixin.connect(AudioStore)
   ],
   componentWillMount: function() {
-    Actions.getSongPreviewUrl(this.state.queue_songs[0]);
+    this.song_ref = new Firebase(fireUrl + '/songs');
+    this.song_ref.on('value', this.getInitSongQueue);
+    // this.getQueueSongs();
   },
   componentWillUpdate: function() {
-    console.log("componentWillUpdate");
-    Actions.loadSong();
+    // console.log('update ' + this.state.song_play_num);
+    this.loadSong();
   },
   render: function() {
-    return <div className="audio-player">
-      <audio id="player-main" controls>
-        <source id="mp3-src" src={this.state.now_playing} type="audio/mpeg" />
+    return <div className="main-audio-player">
+      <p>{this.state.now_playing_track} by {this.state.now_playing_artist}</p>
+      <div className='audio-controls'>
+        <div className="play-btn" onClick={this.handlePlay}>play</div>
+        <div className="pause-btn" onClick={this.handlePause}>pause</div>
+        <div className="next-btn" onClick={this.handleNext}>next</div>
+        <div className="prev-btn" onClick={this.handlePrev}>prev</div>
+      </div>
+      <audio id="player-main">
+        <source id="mp3-src" src={this.state.now_playing_url} type="audio/mpeg" />
       </audio>
     </div>
+  },
+  getInitSongQueue: function(){
+    var songs_queue = [];
+    for(var key in this.state.queue_song_ids){
+      songid = this.state.queue_song_ids[key];
+      this.song_ref.orderByChild('spotify_id').equalTo(songid).once('value', function(songs){
+        songs.forEach(function(song){
+          song= song.val();
+          songs_queue.push({
+            play_url: song.spotify_preview_url,
+            play_track: song.track_name,
+            play_artist: song.artistJoined,
+          });
+        });
+      });
+    }
+    AudioStore.setState({
+      song_queue: songs_queue,
+      song_play_num: 0,
+      now_playing_url: songs_queue[0].play_url,
+      now_playing_track: songs_queue[0].play_track,
+      now_playing_artist: songs_queue[0].play_artist
+    });
+    // AudioStore.setState({song_queue: songs_queue});
+  },
+  loadSong: function() {
+    var audio = document.getElementById('player-main');
+    audio.load();
+  },
+  handlePlay:function() {
+    var audio = document.getElementById('player-main');
+    audio.play();
+  },
+  handlePause:function() {
+    var audio = document.getElementById('player-main');
+    audio.pause();
+  },
+  handleNext:function() {
+    var audio = document.getElementById('player-main');
+    audio.pause();
+    var new_num = this.state.song_play_num + 1;
+    console.log(new_num);
+    AudioStore.setState({
+      song_play_num: new_num,
+      now_playing_url: this.state.song_queue[new_num].play_url,
+      now_playing_track: this.state.song_queue[new_num].play_track,
+      now_playing_artist: this.state.song_queue[new_num].play_artist
+    });
+    //this.loadSong();
+    //audio.play();
+    // console.log(this.state.song_play_num + 1);
+    // var new_num = this.state.song_play_num + 1;
+    // console.log(new_num);
+    // AudioStore.setState({song_play_num: new_num});
+    // console.log('next');
+  },
+  handlePrev:function() {
+    var audio = document.getElementById('player-main');
+    audio.pause();
+    // Audio.setState({song_play_num: })
+    // console.log('prev');
   }
 });
--- src/stores/audioPlayer-store.jsx
+++ src/stores/audioPlayer-store.jsx
@@ -13,28 +13,33 @@ var AudioStore = module.exports = Reflux.createStore({
   listenables: [Actions],
   getInitialState: function(){ 
     return {
-      queue_songs: [
+      queue_song_ids: [
         '1LAzF3sa7k3QrOKtpCVgLJ', //Widowspeak
         '390AWnOn2rfe9FzQjYmxIH', //Chris Isaak
         '2PlGP8KVcHZvayqR8JPLtY', //Future Islands
         '2TaUMeMnlKYhso8C8txtzl' //Amen Dunes
       ],
-      now_playing: ''
+      now_playing_url: '',
+      now_playing_track: '',
+      now_playing_artist: '',
+      song_queue: [],
+      song_play_num: 0
     }
   },
-  getSongPreviewUrl: function(spotify_id){
-    this.song_ref = new Firebase(fireUrl + '/songs');
-    this.song_ref.orderByChild('spotify_id').equalTo(spotify_id).once('value', function(songs){
-      songs.forEach(function(song){
-        song= song.val();
-        AudioStore.setState({now_playing: song.spotify_preview_url});
-      })
-    })
-  },
-  loadSong: function() {
-    var audio = document.getElementById('player-main');
-    audio.load();
-  },
+  // getSongPreviewUrl: function(spotify_id){
+  //   this.song_ref = new Firebase(fireUrl + '/songs');
+  //   this.song_ref.orderByChild('spotify_id').equalTo(spotify_id).once('value', function(songs){
+  //     songs.forEach(function(song){
+  //       song= song.val();
+  //       AudioStore.setState({
+  //         now_playing_url: song.spotify_preview_url,
+  //         now_playing_track: song.track_name,
+  //         now_playing_artist: song.artistJoined
+  //       });
+  //     })
+  //   })
+  // },
+  
   storeDidUpdate: function(prevState) {
     if(this.state.preview_url !== prevState.preview_url){
       //console.log(this.state.preview_url);
